1c8efb4a1dd2478c2d1f7569c93f81e9caf8efcc
Melany Willis <melexiw@gmail.com>
2023-10-04 08:57:13 -0500

    Updated code to add user submission to list

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,24 +3,19 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("events")
+@RequestMapping("events/")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
-    public String events(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+    public String displayAllEvents(Model model) {
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -29,5 +24,11 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 }
